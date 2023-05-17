f492b969bf0206cb325611c8f668c7245e1a075e
Privado11 <wjimenez@unimagdalena.edu.co>
2023-05-17 11:19:44 -0500

    Se agrega el servidor del historial y el botón para el diagrama.

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,13 @@
 const modal = document.getElementById('modalMenu');
+const historial = document.getElementById('history');
+historial.addEventListener("click", openModal);
 const items = document.querySelectorAll('.items');
 const dropdownContent = document.querySelector('.dropdown-content');
+const texto = document.getElementById('texto');
+const cerrarModal = document.getElementById('botonModal');
+cerrarModal.addEventListener("click", closedModal);
+const recorrido = document.getElementById('recorrido');
+recorrido.addEventListener("click", enviarDatos);
 
 loadTranslation('es');
  
@@ -40,4 +47,59 @@ function mostrarValorRango() {
   valor.textContent = rango.value;
 }
 
+function openModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  obtenerDatos(); 
+}
+
+function closedModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+const Palabra={
+  'Palabra':'',
+  'Estado': ''
+}
+
+texto.addEventListener('input',(valor)=>guardarPalabra(valor))
+
+function guardarPalabra(valor){
+  const palabra = valor.target.value;
+  const nuevaPalabra = palabra.replace(/b/g, "a");
+  Palabra.Palabra = palabra;
+  Palabra.Estado = nuevaPalabra;  
+}
+
+function enviarDatos(){
+  fetch('https://desingkuro.pythonanywhere.com/Palabras_recibidas', {
+    method: 'POST',
+    body: JSON.stringify(Palabra),
+    headers: {'Content-Type': 'application/json'}
+  })
+  .then(response => response.text())
+  .then(data => console.log(data))
+}
+
+function obtenerDatos() {
+  let table = document.getElementById("tabla-datos");
+  let rowCount = table.rows.length;
+  for (var i = rowCount - 1; i > 0; i--) {
+      table.deleteRow(i);
+  }
+  
+  fetch('https://desingkuro.pythonanywhere.com/Palabras')
+    .then(response => response.json())
+    .then(data => {
+        const tabla = document.getElementById("tabla-datos");
+        data.forEach((fila) => {
+          const nuevaFila = tabla.insertRow();
+          fila.forEach((datos) => {
+              const nuevaCelda = nuevaFila.insertCell();
+              nuevaCelda.textContent = datos;
+          });
+      });
+    });  
+}
 
